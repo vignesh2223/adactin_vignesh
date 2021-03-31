e5e997b9924bbd10380911a01042a79a17027339
vignesh2223 <vigneshwar.natesan2223@gmail.com>
2021-03-31 14:22:32 +0530

    new commit

---
+++ src/main/java/Cucumber_practice/Cucumber_practice/App.java
@@ -0,0 +1,13 @@
+package Cucumber_practice.Cucumber_practice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
